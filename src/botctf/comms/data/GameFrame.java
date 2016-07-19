0a338398e8d24a8cb126f2d53cfbd2a62b2f064a
Rohan Jhunjhunwala <rjhunjhunwala80@malvernprep.org>
2016-07-19 12:07:37 -0400

    Send the whole map to the player each time

--- src/botctf/comms/data/GameFrame.java
+++ src/botctf/comms/data/GameFrame.java
@@ -8,41 +8,36 @@ public class GameFrame {
 
 	private Player player;
 
-
-	public GameFrame(Player player)
-	{
+	public GameFrame(Player player) {
 		this.player = player;
 	}
-	
-	public void toMessage(StringBuilder sb)
-	{
-		sb.append("FRAME"); sb.append("\n");
-		sb.append(Arena.getAmmo()[player.team]); sb.append("\n");
-		sb.append(Arena.getAliveBots()); sb.append("\n");
-		sb.append(player.getX()); sb.append(","); sb.append(player.getY()); sb.append("\n");
+
+	public void toMessage(StringBuilder sb) {
+		sb.append("FRAME");
+		sb.append("\n");
+		sb.append(Arena.getAmmo()[player.team]);
+		sb.append("\n");
+		sb.append(Arena.getAliveBots());
+		sb.append("\n");
+		sb.append(player.getX());
+		sb.append(",");
+		sb.append(player.getY());
+		sb.append("\n");
 		appendLocalMap(sb);
-		sb.append("EMARF"); sb.append("\n");
-		sb.setLength(sb.length()-1); // sending message already appends new line character
+		sb.append("EMARF");
+		sb.append("\n");
+		sb.setLength(sb.length() - 1); // sending message already appends new line character
 	}
-	
-	private void appendLocalMap(StringBuilder sb)
-	{
+
+	private void appendLocalMap(StringBuilder sb) {
 		int[][] map = player.getMap();
 		final int maxX = map[0].length;
 		final int maxY = map.length;
-		for (int y=player.getY()-2;y<=player.getY()+2;y++)
-		{
-			for (int x=player.getX()-2;x<=player.getX()+2;x++)
-			{
-				if (x>-1 && x < maxX && y >-1 && y< maxY)
-				{
-					sb.append((char) map[x][y]);
-				}
-				else
-				{
-					// show out of arena as wall
-					sb.append((char) Bot.wall);
-				}
+		for (int y = 0; y <maxY; y++) {
+			for (int x = 0; x < maxX; x++) {
+
+				sb.append((char) map[x][y]);
+
 			}
 			sb.append("\n");
 		}
